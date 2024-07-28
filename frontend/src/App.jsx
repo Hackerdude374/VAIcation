f80d844c9c03cdcd9906ae77c3c9db3f100a3946
Robert Le <robertle982@gmail.com>
2024-07-28 13:16:05 -0400

    button

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import {Button} from './components/ui/button'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,6 +10,7 @@ function App() {
   return (
     <>
     welcome to robert
+    <Button>Subscribe</Button>
     </>
   )
 }
