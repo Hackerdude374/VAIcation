22f6a503fcd24860b83acb32998cdae010e28e5c
Robert Le <robertle982@gmail.com>
2024-07-28 20:06:33 -0400

    trip form

--- frontend/src/create-trip/CreateTripForm.jsx
+++ frontend/src/create-trip/CreateTripForm.jsx
@@ -1,11 +1,11 @@
-import React, { useState } from 'react'
-import GooglePlacesAutocomplete from 'react-google-places-autocomplete'
-import { Input } from "@/components/ui/input"
-import { SelectBudgetOptions } from '@/constants/options'
+import React, { useState } from "react";
+import GooglePlacesAutocomplete from "react-google-places-autocomplete";
+import { Input } from "@/components/ui/input";
+import { SelectBudgetOptions, SelectTravelesList } from "@/constants/options";
 
 function CreateTrip() {
   const [place, setPlace] = useState();
-  const [days, setDays] = useState('');
+  const [days, setDays] = useState("");
 
   const handleDaysChange = (e) => {
     const value = Math.max(0, Math.min(365, Number(e.target.value)));
@@ -13,29 +13,36 @@ function CreateTrip() {
   };
 
   return (
-    <div className='sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10'>
-      <h2 className='font-bold text-3xl'>
+    <div className="sm:px-10 md:px-32 lg:px-56 xl:px-10 px-5 mt-10">
+      <h2 className="font-bold text-3xl">
         Tell us your travel preferences.
-        <p className='mt-3 text-gray-500 text-xl'>
+        <p className="mt-3 text-gray-500 text-xl">
           Just provide some basic information, and our trip planner
         </p>
       </h2>
-      <div className='mt-20 flex flex-col gap-10'>
+      <div className="mt-20 flex flex-col gap-10">
         <div>
-          <h2 className='text-xl my-3 font-medium'>What is destination of choice?</h2>
+          <h2 className="text-xl my-3 font-medium">
+            What is destination of choice?
+          </h2>
           <GooglePlacesAutocomplete
             apiKey={import.meta.env.VITE_GOOGLE_PLACE_API_KEY}
             selectProps={{
               value: place,
-              onChange: (v) => { setPlace(v); console.log(v) }
+              onChange: (v) => {
+                setPlace(v);
+                console.log(v);
+              },
             }}
           />
         </div>
         <div>
-          <h2 className='text-xl my-3 font-medium'>How many days is your trip?</h2>
-          <Input 
-            placeholder={'Ex. 3'} 
-            type="number" 
+          <h2 className="text-xl my-3 font-medium">
+            How many days is your trip?
+          </h2>
+          <Input
+            placeholder={"Ex. 3"}
+            type="number"
             min="0"
             max="365"
             value={days}
@@ -43,19 +50,53 @@ function CreateTrip() {
           />
         </div>
       </div>
+
+      <div>
       {/* these options are made from my objects array in constants/options.jsx */}
-      <h2 className='text-xl my-3 font-medium'>What is your budget?</h2>
-      <div className='grid grid-cols-3 gap-5 mt-5'>
+      <h2 className="text-xl my-3 font-medium">What is your budget?</h2>
+      <div className="grid grid-cols-3 gap-5 mt-5">
+     
         {SelectBudgetOptions.map((item, index) => (
-          <div key={index} className='p-4 border cursor-pointer rounded-lg hover:shadow-lg'>
-            <h2 className='text-4xl'>{item.icon}</h2>
-            <h2 className='font-bold text-lg'>{item.title}</h2>
-            <h2 className ='text-sm text-gray-500'>{item.desc}</h2>
+          <div
+            key={index}
+            className="p-4 border cursor-pointer rounded-lg hover:shadow-lg"
+          >
+            <h2 className="text-4xl">{item.icon}</h2>
+            <h2 className="font-bold text-lg">{item.title}</h2>
+            <h2 className="text-sm text-gray-500">{item.desc}</h2>
           </div>
         ))}
       </div>
     </div>
-  )
+
+
+    <div>
+      {/* these options are made from my objects array in constants/options.jsx */}
+      <h2 className="text-xl my-3 font-medium">Who do you plan on traveling with on your next adventurw??</h2>
+      <div className="grid grid-cols-3 gap-5 mt-5">
+     
+        {SelectTravelesList.map((item, index) => (
+          <div
+            key={index}
+            className="p-4 border cursor-pointer rounded-lg hover:shadow-lg"
+          >
+            <h2 className="text-4xl">{item.icon}</h2>
+            <h2 className="font-bold text-lg">{item.title}</h2>
+            <h2 className="text-sm text-gray-500">{item.desc}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
+
+
+
+
+
+
+
+
+    </div>
+  );
 }
 
-export default CreateTrip
+export default CreateTrip;
